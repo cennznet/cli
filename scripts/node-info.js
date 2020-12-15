d187c453f13691dc3123d688904f339489d9fdd0
holygits <beauchjord@gmail.com>
2020-12-15 13:47:09 +1300

    add node info script

---
+++ scripts/node-info.js
@@ -0,0 +1,23 @@
+console.log("**********");
+console.log("\u001b[34mNode Info\u001b[37m");
+console.log("**********");
+
+api.rpc.system.chain().then(c => console.log(`chain: ${c.toHuman()}`));
+api.rpc.system.nodeRoles().then(r => console.log(`role: ${r[0]}`));
+api.rpc.system.version().then(v => console.log(`client version: ${v.toHuman()}`));
+api.rpc.system.localPeerId().then(p => console.log(`peer id: ${p.toHuman()}`));
+
+activeSessionKeys = (await api.query.session.queuedKeys());
+activeSessionKeys.map(async ([stash, [gran, audi, imon, babe]]) => {
+    let match = [
+        (await api.rpc.author.hasKey(babe, 'babe')).toHuman(),
+        (await api.rpc.author.hasKey(imon, 'imon')).toHuman(),
+        (await api.rpc.author.hasKey(audi, 'audi')).toHuman(),
+        (await api.rpc.author.hasKey(gran, 'gran')).toHuman(),
+    ];
+    if (match) {
+        console.log(">>> \u001b[32mACTIVE\u001b[37m validator <<<");
+        console.log(`connected to stash 💰: ${stash}\nwith session keys: \n babe 👶: ${babe}\n imon 💓: ${imon}\n audi 🚘: ${audi}\n gran 👴: ${gran}\n`);
+        return
+    }
+});
--- scripts/subscription.js
+++ scripts/subscription.js
@@ -1,1 +1,7 @@
-await api.query.syloDevice.devices('5CUHuuq7jFMqHtVMXiEMucX7vU69jbnrCqMWZ6F8Jq4XyZaK', x => console.log(x));
+await api.query.syloDevice.devices('5CUHuuq7jFMqHtVMXiEMucX7vU69jbnrCqMWZ6F8Jq4XyZaK', x => console.log(x.toJSON()));
+
+let tx = api.tx.system.remark('0x1234');
+let result = tx.signAndSend(toyKeyring.alice);
+console.log(result.toJSON());
+console.log((await api.query.system.accountNonce(toyKeyring.alice.address)).toJSON());
+
