0415efe3631f53d7662ef3068e9a92d9b7cb546a
holygits <beauchjord@gmail.com>
2020-12-15 14:39:25 +1300

    node-info exit

--- scripts/node-info.js
+++ scripts/node-info.js
@@ -8,16 +8,19 @@ api.rpc.system.version().then(v => console.log(`client version: ${v.toHuman()}`)
 api.rpc.system.localPeerId().then(p => console.log(`peer id: ${p.toHuman()}`));
 
 activeSessionKeys = (await api.query.session.queuedKeys());
-activeSessionKeys.map(async ([stash, [gran, audi, imon, babe]]) => {
-    let match = [
-        (await api.rpc.author.hasKey(babe, 'babe')).toHuman(),
-        (await api.rpc.author.hasKey(imon, 'imon')).toHuman(),
-        (await api.rpc.author.hasKey(audi, 'audi')).toHuman(),
-        (await api.rpc.author.hasKey(gran, 'gran')).toHuman(),
-    ];
-    if (match.every(v => v == true)) {
-        console.log(">>> \u001b[32mACTIVE\u001b[37m validator <<<");
-        console.log(`connected to stash 💰: ${stash}\nwith session keys: \n babe 👶: ${babe}\n imon 💓: ${imon}\n audi 🚘: ${audi}\n gran 👴: ${gran}\n`);
-        return
-    }
-});
+await Promise.all(
+    activeSessionKeys.map(async ([stash, [gran, audi, imon, babe]]) => {
+        let match = [
+            (await api.rpc.author.hasKey(babe, 'babe')).toHuman(),
+            (await api.rpc.author.hasKey(imon, 'imon')).toHuman(),
+            (await api.rpc.author.hasKey(audi, 'audi')).toHuman(),
+            (await api.rpc.author.hasKey(gran, 'gran')).toHuman(),
+        ];
+        if (match.every(v => v == true)) {
+            console.log(">>> \u001b[32mACTIVE\u001b[37m validator <<<");
+            console.log(`connected to stash 💰: ${stash}\nwith session keys: \n babe 👶: ${babe}\n imon 💓: ${imon}\n audi 🚘: ${audi}\n gran 👴: ${gran}\n`);
+        }
+    })
+);
+
+process.exit();
